907eeae6a675067f1da014e644a86aac723f71c7
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-05-02 12:29:34 +0200

    Fix toast messages

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Home from './Home.jsx';
-import MapView from './MapView.jsx';
 import LogIn from './LogIn.jsx';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import MapToolPage from './Pages/MapToolPage.jsx'
+import MapToolPage from './Pages/MapToolPage.jsx';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Toaster />
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={< LogIn />} />
@@ -18,5 +19,5 @@ createRoot(document.getElementById('root')).render(
         <Route path="/app" element={<App />} />
       </Routes>
     </BrowserRouter>
-</StrictMode>,
+  </StrictMode>,
 );
