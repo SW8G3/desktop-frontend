013087508e98b749cc677b7b4784639b679bc1d7
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-05-07 10:05:21 +0200

    Fix another one lol

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -55,7 +55,7 @@ const LogIn = () => {
         setMsgColor(''); // Clear message color
 
         try {
-            const response = await fetch(`${import.meta.env.VITE_ADMIN_API_URL}/login/login`, {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/login/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
